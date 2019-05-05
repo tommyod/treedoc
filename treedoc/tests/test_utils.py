c79255262bf82845fd4708b4c206798ef38fd92c
tommyod <tommy.odland@gmail.com>
2019-05-05 17:54:20 +0200

    ran black

--- treedoc/tests/test_utils.py
+++ treedoc/tests/test_utils.py
@@ -9,8 +9,7 @@ Created on Sun Apr 28 20:20:49 2019
 import operator
 
 import treedoctestpackage
-from treedoc.utils import (descend_from_package, get_docstring, ispackage,
-                           resolve_object)
+from treedoc.utils import descend_from_package, get_docstring, ispackage, resolve_object
 
 
 def map_itemgetter(iterable, index):
