0bc4b09de0d5e76382e57c25e8435962f6c7683f
Sean Meling Murray <sean.meling@gmail.com>
2019-05-19 13:07:54 +0200

    added maintainer, maintainer_email, keywords, url

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+from os import path
 from setuptools import find_packages, setup
+
+# Get the long description from README.md
+PATH = path.abspath(path.dirname(__file__))
+with open(path.join(PATH, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="treedoc",
     version="0.2.0",
-    description="Minimalistic Python documentation in a tree structure.",
-    long_description="Minimalistic Python documentation in a tree structure.",
+    description="Minimalistic Python documentation for dendrophiles.",
+    long_description=long_description,
+    url="https://github.com/tommyod/treedoc",
     author="tommyod",
     author_email="tod001@uib.no",
+    maintainer="tommyod, smu095, glemvik",
+    maintainer_email="treedoc.dev@gmail.com",
     license="MIT",
     packages=find_packages(exclude=["*tests*"]),
     python_requires=">=3.6",
@@ -18,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords=["documentation"],
+    keywords=["documentation, utility"],
 )
