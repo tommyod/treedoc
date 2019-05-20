539096eee6b1ebe380159120bac1bacc2264ed04
tommyod <tommy.odland@gmail.com>
2019-05-20 18:42:37 +0200

    ignored __init__ in flake, removed docker in travis

--- treedoc/__init__.py
+++ treedoc/__init__.py
@@ -4,10 +4,6 @@
 Minimalistic Python documentation for dendrophiles.
 """
 
-
 from treedoc.main import treedoc
-
-# This is to avoid error: F401 'treedoc.main.treedoc' imported but unused
-treedoc = treedoc
 
 __version__ = "0.3.0"
