e5cf8bc8c1c2fb8ed95f2d80c4995ea86e9e7190
smu095 <sean.meling@gmail.com>
2019-07-05 18:52:48 +0200

    [MRG] Optimising recursion (#35)
    * optimised recurse_to_object method of ObjectTraverser
    
    * ran black
    
    * reverted to return False statements
    
    * added inline comments to recurse_to_object
    
    * ran black

--- treedoc/traversal.py
+++ treedoc/traversal.py
@@ -224,31 +224,40 @@ class ObjectTraverser(PrintMixin):
     def recurse_to_object(self, obj) -> bool:
         """Given an object, should we recurse down to it?"""
 
-        # TODO: Optimize this by placing what fails most often first
-
         name = obj.__name__
 
-        if name in self._ignored_names:
+        # The following order is based on line profiling in an attempt to speed
+        # up recursion
+        # Inline comments original condition -> renamed condition indicate
+        # renaming condition following profiling
+
+        # 1.5 -> 1.1
+        if is_dunder_method(obj) and not self.dunders:
             self._p(f"O: Failed on condition 1.1")
             return False
 
-        if obj is type:
+        # 1.4 -> 1.2
+        if is_private(obj) and not self.private:
             self._p(f"O: Failed on condition 1.2")
             return False
 
-        if is_test(obj) and not self.tests:
+        # 1.2 -> 1.3
+        if obj is type:
             self._p(f"O: Failed on condition 1.3")
             return False
 
-        if is_private(obj) and not self.private:
+        # 1.1 -> 1.4
+        if name in self._ignored_names:
             self._p(f"O: Failed on condition 1.4")
             return False
 
-        if is_dunder_method(obj) and not self.dunders:
+        # 1.6 -> 1.5
+        if not is_inspectable(obj):
             self._p(f"O: Failed on condition 1.5")
             return False
 
-        if not is_inspectable(obj):
+        # 1.3 -> 1.6
+        if is_test(obj) and not self.tests:
             self._p(f"O: Failed on condition 1.6")
             return False
 
